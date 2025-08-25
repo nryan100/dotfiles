2f270e589ed9bf85d138ef0e758fdbb1b35e5d8e
Nicholas <nicholasryan100@gmail.com>
2025-08-25 14:16:09 -0600

    remove append for root user

--- apply-appends.sh
+++ apply-appends.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 CONTENT="source <(curl -s https://raw.githubusercontent.com/nryan100/dotfiles/refs/heads/main/rc-appends)"
-su root
-echo $CONTENT >> /root/.bashrc
-exit
 echo $CONTENT >> $HOME/.bashrc
 source $HOME/.bashrc
